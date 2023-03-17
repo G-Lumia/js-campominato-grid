41dd6b2a0015489c9a024fdd69dff28b9a51f7c6
G-Lumia <giorgiolumia@gmail.com>
2023-03-17 16:37:02 +0100

    grid function and square function created

--- js/myscript.js
+++ js/myscript.js
@@ -8,3 +8,55 @@
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 /* <div class="square"> 1 </div> */
+
+const playButton = document.getElementById("startGame");
+const game = document.getElementById("gameSquare");
+const start = document.getElementById("startMessage");
+
+playButton.addEventListener("click", gameStart);
+
+// funzione usata per dare in output un quadrato con un numero scritto al suo interno e di una dimesione specifica in base alla difficoltà scelta
+function drawSquare(index, numSquares)
+{
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `calc(100% / ${numSquares})`;
+    square.style.heigth = square.style.width;
+    square.innerHTML = index + 1;
+    return square;
+}
+
+// funzione usata per creare l'area di gioco
+
+function gameStart()
+{
+    game.innerHTML = ``;
+    const difficulty = document.getElementById("difficulty").value;
+    let squareNumbers;
+    let squareForRow;
+    switch(difficulty)
+    {
+        case "facile" :
+            squareNumbers = 100;
+            squareForRow = 10;
+            break;
+        case "medio" :
+            squareNumbers = 81;
+            squareForRow = 9;
+            break;
+        case "difficile" :
+            squareNumbers = 49;
+            squareForRow = 7;
+            break;
+    }
+
+    // ciclo iterativo dove viene costruita l'area di gioco
+    for(let i=0 ; i<squareNumbers ; i++)
+    {
+        const square = drawSquare(i , squareForRow);
+        game.appendChild(square);
+    }
+
+    game.classList.remove("d-none");
+    start.classList.add("d-none");
+}
